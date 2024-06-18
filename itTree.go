981838aa416fb573a53db7af445083a5932368db
d <d.zidarov@gmail.com>
2024-06-18 06:40:55 -0400

    iterative tree debugged

--- itTree.go
+++ itTree.go
@@ -47,27 +47,65 @@ func insertNode(n *Node, key int) *Node {
 		}
 		for i := len(v) - 1; i >= 0; i-- {
 			node := v[i]
-			//  fmt.Println(node.height)
-			//	fmt.Println(node, ": ",height(node.left), "-", height(node.right))
-			//	fmt.Println(newHeight(node))
 			node.height = newHeight(node)
 			balance := height(node.left) - height(node.right)
 			switch {
 			case balance > 1:
-				fmt.Println("Looking at: ", node)
-				if node.left.right == nil {
-					rotateRight(node)
-				} else if node.left.left == nil {
-					rotateLeft(node.left)
-					rotateRight(node)
+				// base case: if node.left.right is nil then the nodes are
+				//lined up and only one rotation is necessary. Using the height
+				//of the branches generalizes the base case
+				if height(node.left.left) > height(node.left.right) {
+					if i == 0 {
+						//if your are rotating the root node set the return to 
+						//the new root after rotating
+						n = rotateRight(node)
+					} else if i > 0 {
+						//if your are not at the root node go one level up and 
+						//set the previous left || right node to the new one
+						if v[i-1].left == node {
+							v[i-1].left = rotateRight(node)
+						} else if v[i-1].right == node {
+							v[i-1].right = rotateRight(node)
+						}
+					}
+				// base case: if node.left.left is nil then the nodes are
+				//stagared and two rotations are necessary. Using the height
+				//of the branches generalizes the base case
+				} else if height(node.left.left) < height(node.left.right) {
+					node.left = rotateLeft(node.left)
+					if i == 0 {
+						n = rotateRight(node)
+					} else if i > 0 {
+						if v[i-1].left == node {
+							v[i-1].left = rotateRight(node)
+						} else if v[i-1].right == node {
+							v[i-1].right = rotateRight(node)
+						}
+					}
 				}
 
 			case balance < -1:
-				if node.right.left == nil {
-					rotateLeft(node)
-				} else if node.left.right == nil {
-					rotateRight(node.right)
-					rotateLeft(node)
+				if height(node.right.right) > height(node.right.left) {
+					if i == 0 {
+						n = rotateLeft(node)
+					} else if i > 0 {
+						if v[i-1].left == node {
+							v[i-1].left = rotateLeft(node)
+						} else if v[i-1].right == node {
+							v[i-1].right = rotateLeft(node)
+						}
+					}
+				} else if height(node.right.right) < height(node.right.left) {
+					node.right = rotateRight(node.right)
+					if i == 0 {
+						n = rotateLeft(node)
+					} else if i > 0 {
+						if v[i-1].left == node {
+							v[i-1].left = rotateLeft(node)
+						} else if v[i-1].right == node {
+							v[i-1].right = rotateLeft(node)
+						}
+					}
 				}
 			}
 		}
@@ -75,26 +113,24 @@ func insertNode(n *Node, key int) *Node {
 	return n
 }
 
-func rotateRight(n *Node) {
+func rotateRight(n *Node) *Node {
 	t := n.left
-	t1 := n.left.right
+	t1 := t.right
 	t.right = n
 	n.left = t1
 	n.height = newHeight(n)
 	t.height = newHeight(t)
-	fmt.Println("*******")
-	fmt.Println(t)
-	fmt.Println(n)
-	fmt.Println("*******")
+	return t
 }
 
-func rotateLeft(n *Node) {
+func rotateLeft(n *Node) *Node {
 	t := n.right
 	t1 := n.right.left
 	t.left = n
 	n.right = t1
 	n.height = newHeight(n)
 	t.height = newHeight(t)
+	return t
 }
 
 func newHeight(n *Node) int {
@@ -131,11 +167,37 @@ func printTree(root *Node, indent string, last bool) {
 	}
 }
 
+func printNode(n *Node) {
+	v := make([]*Node, 0)
+	v = append(v, n)
+
+	var bWT func(n *Node)
+	bWT = func(n *Node) {
+		if n == nil {
+			return
+		}
+		if n.left != nil {
+			v = append(v, n.left)
+		}
+		if n.right != nil {
+			v = append(v, n.right)
+		}
+		bWT(n.left)
+		bWT(n.right)
+	}
+	bWT(n)
+
+	for _, k := range v {
+		fmt.Println(k.key)
+	}
+}
+
 func main() {
-	v := []int{3, 2, 1}
+	v := []int{1, 5, 8, 6, 9, 7,10,11,12,13,14}
 	var t *Node
 	for _, k := range v {
 		t = insertNode(t, k)
 	}
 	printTree(t, "", true)
+	// printNode(t)
 }
